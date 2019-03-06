6f0bab73dc305ad0b4eadf9c4a221fdeee3f7256
thaycacac <thaycacac@gmail.com>
2019-03-06 21:39:10 +0700

    :bulb: update navigation

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,5 @@
 export { default as Navigation } from './Navigation'
 
-export { default as Button } from './Button'
-
 export { default as SideBarSmall } from './SideBarSmall'
 
 export { default as SideBarFull } from './SideBarFull'
