8359a849a0f10dcbcbc3da9aaa8fc503ccdd37f4
millennium <hoangmamhi666@gmail.com>
2019-03-07 20:32:21 +0700

    add left side bar

--- src/components/SideBarFull.tsx
+++ src/components/SideBarFull.tsx
@@ -1,20 +1,58 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import {
+  IconBookMark as bookmark,
+  IconTime as date
+} from '../images'
 
 const WrapContent = styled.div`
   .content {
-    background-color: pink;
-    color: white;
-    text-align: center;
+    	padding: 50px;
+
+    .box {
+      border: 0px solid;
+      background-color: #FFFFFF;
+      margin-bottom: 1.5rem;
+      box-shadow: 0 2px 10px grey;
+      border-radius: 5px;
+      width: 150px;
+      height: auto;
+
+      .box-content {
+        padding-top: 6px;
+        padding-left: 8px;
+        background-color: #FFFFFF;
+        width: 150px;
+        height: 45px;
+        border-radius: 5px;
+        color: grey;
+      }
+    }
   }
 `
 
 function SideBar() {
-  return(
+  return (
     <WrapContent>
-      <p className="content">
-        Hoàng
-      </p>
+      <div className="content">
+        <div className="col-lg">
+          <div className="box">
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/profile.png')}></img>  My Quiz</div>
+            </Link>
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/global.png')}></img>  Discover</div>
+            </Link>
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/setting.png')}></img>  Settings</div>
+            </Link>
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/infor.png')}></img>  Support</div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </WrapContent>
   )
 }
--- src/components/SideBarSmall.tsx
+++ src/components/SideBarSmall.tsx
@@ -1,20 +1,69 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import {
+  IconBookMark as bookmark,
+  IconTime as date
+} from '../images'
 
 const WrapContent = styled.div`
   .content {
-    background-color: pink;
-    color: white;
-    text-align: center;
+    padding: 50px;
+
+    .box {
+      border: 0px solid;
+      background-color: #FFFFFF;
+      margin-bottom: 1.5rem;
+      box-shadow: 0 2px 10px grey;
+      border-radius: 5px;
+      width: 40px;
+      height: auto;
+
+      .box-content {
+        padding-top: 6px;
+        padding-left: 8px;
+        background-color: #FFFFFF;
+        border-radius: 5px;
+        width: 40px;
+        height: 45px;
+        color: grey;
+      }
+    }
   }
 `
 
 function SideBar() {
-  return(
+  return (
     <WrapContent>
-      <p className="content">
-        Hoàng
-      </p>
+      <div className="content">
+        <div className="col-lg">
+          <div className="box">
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/profile.png')}></img></div>
+            </Link>
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/global.png')}></img></div>
+            </Link>
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/setting.png')}></img></div>
+            </Link>
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/infor.png')}></img></div>
+            </Link>
+          </div>
+        </div>
+
+        <div className="col-lg">
+          <div className="box">
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/setting.png')}></img></div>
+            </Link>
+            <Link to="#">
+              <div className="box-content"><img src={require('../images/icon/infor.png')}></img></div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </WrapContent>
   )
 }
