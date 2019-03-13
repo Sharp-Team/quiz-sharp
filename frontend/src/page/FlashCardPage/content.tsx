56bdd1ed779426d9b9ae592eb1cd018dd8d3b678
thaycacac <thaycacac@gmail.com>
2019-03-13 23:17:57 +0700

    :tada: add: flash-card

--- frontend/src/page/FlashCardPage/content.tsx
+++ frontend/src/page/FlashCardPage/content.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import Slider from 'react-slick'
+import './styles.css'
 
 const WrapContent = styled.div`
     .warp-quiz {
@@ -37,29 +39,63 @@ const WrapContent = styled.div`
     }
 `
 
-const flip = (): void => {
-    const quiz = document.getElementsByClassName('warp-quiz')[0]
-    if(!quiz.classList.contains('flip')) {
-        quiz.classList.add('flip')
-    } else {
-        quiz.classList.remove('flip')
-    }        
+const flip = (quizId: string): void => {
+    const quiz = document.getElementById('flip-' + quizId)
+    if (quiz !== null) {
+        if(!quiz.classList.contains('flip')) {
+            quiz.classList.add('flip')
+        } else {
+            quiz.classList.remove('flip')
+        } 
+    }
 }
 
-function Content() {
-    return(
-        <WrapContent>
-            <div className="container-fluid warp-quiz" onClick={(): void => flip()}>
-                <span className="text-front">Cong hoa xa hoi chu nghia Viet Nam</span>
-                <span className="text-back">Doc lap tu do hanh phuc</span>
-            </div>
-            <div className="slider demo">
-                <div> Slide  1 </div>
-                <div> Slide  2 </div>
-                <div> Slide  3 </div>
-            </div>
+const hander = (event: any): void => { 
+    if(event.key === ' ') {
+        const quiz = document.getElementsByClassName('slick-active')[0].children[0].children[0].children[0].id.split('-')[1]
+        flip(quiz)
+    }
+}
 
-        </WrapContent>
+function Content({ listQuiz, changeQuizCurrent }: any) {
+    var settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+      }
+      window.addEventListener('load', function () {
+        const quiz = document.getElementsByClassName('slick-active')[0].children[0].children[0].children[0].id.split('-')[1]
+        changeQuizCurrent(quiz)
+
+        const slickPrev = document.getElementsByClassName('slick-prev')[0]
+        slickPrev.addEventListener('click', function() {changeQuizCurrent(
+            parseInt(document.getElementsByClassName('slick-active')[0].children[0].children[0].children[0].id.split('-')[1]) - 1
+            ) })
+
+        const slickNext = document.getElementsByClassName('slick-next')[0]
+        slickNext.addEventListener('click', function() {changeQuizCurrent(
+            parseInt(document.getElementsByClassName('slick-active')[0].children[0].children[0].children[0].id.split('-')[1]) + 1
+            ) })
+      })
+
+    return(
+        <div onKeyPress={hander}>
+            <Slider {...settings} className="wrap-slick">
+                {listQuiz.map((quiz: any) => (
+                    <WrapContent key={quiz.id} className="px-5">
+                        <div
+                            className="warp-quiz"
+                            onClick={(): void => { flip(quiz.id); changeQuizCurrent(quiz.id) }}
+                            id={`flip-${quiz.id}`}
+                        >
+                            <span className="text-front">{ quiz.term }</span>
+                            <span className="text-back">{ quiz.definition }</span>
+                        </div>
+                    </WrapContent>
+                ))}
+            </Slider>
+            </div>
     )
 }
 
--- frontend/src/page/FlashCardPage/index.tsx
+++ frontend/src/page/FlashCardPage/index.tsx
@@ -3,24 +3,47 @@ import { Navigation } from '../../components'
 import Content from './content'
 import SideBar from './sidebar'
 
-function FlashCardPage() {
-  const listQuiz = [
-    { id: 1, term: "hello", definition: "xin chao"  },
-    { id: 2, term: "goodby", definition: "tam biet" },
-    { id: 3, term: "sorry", definition: "xin loi" }
-  ]
+
+class FlashCardPage extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      quizCurrent: 0,
+      listQuiz: [
+        { id: 1, term: "hello", definition: "xin chao"  },
+        { id: 2, term: "goodby", definition: "tam biet" },
+        { id: 3, term: "sorry", definition: "xin loi" },
+        { id: 4, term: "cong hoa xa hoi chu nghia viet nam", definition: "doc lap tu do hanh phuc" }
+      ]
+    }
+  }
+
+  changeQuizCurrent = (quizId: number) => {
+    this.setState ({
+      quizCurrent: quizId
+    })
+  }
   
-  return (
-    <div>
-      <Navigation />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-2"><SideBar /></div>
-          <div className="col-10"><Content /></div>
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-2"><SideBar
+              listQuiz={this.state.listQuiz}
+              quizCurrent={this.state.quizCurrent}/></div>
+            <div className="col-10">
+              <Content
+                listQuiz={this.state.listQuiz}
+                changeQuizCurrent={this.changeQuizCurrent}/>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default FlashCardPage
